4fba61e7f5d2af3deb947ed96d350bff9282b2d7
imran <zunaeede@gmail.com>
2023-09-09 18:18:15 +0600

    main - asci value  add - asci value addin switch statement

---
+++ 3_ASCI_value.js
@@ -0,0 +1,85 @@
+let asci = prompt("Enter any Alphabet to see ASCI Value of it");
+
+switch(asci){
+    case asci = "a":
+        document.write(asci+" ASCI Value is 097 ");
+        break;
+    case asci = "b":
+        document.write(asci+" ASCI Value is 098");
+        break;
+    case asci = "c":
+        document.write(asci+" ASCI Value is 099");
+        break;
+    case asci = "d":
+        document.write(asci+" ASCI Value is 100");
+        break;
+    case asci = "e":
+        document.write(asci+" ASCI Value is 101");
+        break;
+    case asci = "f":
+        document.write(asci+" ASCI Value is 102");
+        break;
+    case asci = "g":
+        document.write(asci+" ASCI Value is 103");
+        break;
+    case asci = "h":
+        document.write(asci+" ASCI Value is 104");
+        break;
+    case asci = "i":
+        document.write(asci+" ASCI Value is 105");
+        break;
+    case asci = "j":
+        document.write(asci+" ASCI Value is 106");
+        break;
+    case asci = "k":
+        document.write(asci+" ASCI Value is 107");
+        break;
+    case asci = "l":
+        document.write(asci+" ASCI Value is 108");
+        break;
+    case asci = "m":
+        document.write(asci+" ASCI Value is 109");
+        break;
+    case asci = "n":
+        document.write(asci+" ASCI Value is 110");
+        break;
+    case asci = "o":
+        document.write(asci+" ASCI Value is 111");
+        break;
+    case asci = "p":
+        document.write(asci+" ASCI Value is 112");
+        break;
+    case asci = "q":
+        document.write(asci+" ASCI Value is 113");
+        break;
+    case asci = "r":
+        document.write(asci+" ASCI Value is 114");
+        break;
+    case asci = "s":
+        document.write(asci+" ASCI Value is 115");
+        break;
+    case asci = "t":
+        document.write(asci+" ASCI Value is 116");
+        break;
+    case asci = "u":
+        document.write(asci+" ASCI Value is 117");
+        break;
+    case asci = "v":
+        document.write(asci+" ASCI Value is 118");
+        break;
+    case asci = "w":
+        document.write(asci+" ASCI Value is 119");
+        break;
+    case asci = "x":
+        document.write(asci+" ASCI Value is 120");
+        break;
+    case asci = "y":
+        document.write(asci+" ASCI Value is 121");
+        break;
+    case asci = "z":
+        document.write(asci+" ASCI Value is 122");
+        break;
+    default:
+        document.write("It's Not an Alphabet");    
+            
+}
