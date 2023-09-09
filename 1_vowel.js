2b25d1ef7139e0a62fc7d7a3c6908257b1102992
imran <zunaeede@gmail.com>
2023-09-09 15:27:01 +0600

    main - switch case all - alphabet add

--- 1_vowel.js
+++ 1_vowel.js
@@ -2,10 +2,35 @@ let num = prompt("Entera any Alphabet");
 
 switch (num){
     case num = "a":
+    case num = "e":
+    case num = "i":
+    case num = "o":
+    case num = "u":
         document.write("It is Vowel");
         break;
     case num = "b":
-        document.write("Not vowel");
+    case num = "c":
+    case num = "d":
+    case num = "e":
+    case num = "f":
+    case num = "g":
+    case num = "h":
+    case num = "j":
+    case num = "k":
+    case num = "l":
+    case num = "m":
+    case num = "p":
+    case num = "q":
+    case num = "r":    
+    case num = "s":
+    case num = "t":
+    case num = "v":
+    case num = "w":
+    case num = "x":
+    case num = "y":
+    case num = "z":
+    document.write("Not vowel");
+
     default:
         document.write("Wrong Entry");    
     }    
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Basic_Problem_solve
 #### basic problem solving practice
-1. vowel and consonent test practice
+1. vowel and consonent will show the entry.
 - in switch statement condation must be "=" only don't need "===" or "==" (switch)
 - if we use "===" or "==" in condation not gonna work (switch)
