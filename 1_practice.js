aabe0caf84d1fcd673986edf0ca399d466ca6661
imran <zunaeede@gmail.com>
2023-09-09 17:44:50 +0600

    practice - alphabet practice with if else or

---
+++ 1_practice.js
@@ -0,0 +1,7 @@
+let alpha = prompt("Enter a alphabet"); 
+
+if( alpha === "a" || alpha === "b" || alpha === "c" || alpha === "d" || alpha === "e" || alpha === "f" || alpha === "g" || alpha === "h" || alpha === "i" || alpha === "j" || alpha === "k" || alpha === "l" || alpha === "m" || alpha === "n" || alpha === "o" || alpha === "p" || alpha === "q" || alpha === "r" || alpha === "s" || alpha === "t" || alpha === "u" || alpha === "v" || alpha === "w" || alpha === "x" || alpha === "y" || alpha === "z"){
+    document.write(alpha + " is Alphabet");
+} else {
+    document.write("Not alphabet");
+}
