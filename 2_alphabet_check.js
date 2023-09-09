f15fe7e3169b1cbe33a989bb486562e86b09c9ac
imran <zunaeede@gmail.com>
2023-09-09 16:30:00 +0600

    main - alphabet - switch condation - switch statement use

--- 2_alphabet_check.js
+++ 2_alphabet_check.js
@@ -2,6 +2,32 @@ let alpha = prompt("Enter any Alphabet");  // const won't work in this case.
 
 switch(alpha){
     case alpha = "a":
+    case alpha = "b":
+    case alpha = "c":
+    case alpha = "d":
+    case alpha = "e":
+    case alpha = "f":
+    case alpha = "g":
+    case alpha = "h":
+    case alpha = "i":
+    case alpha = "j":
+    case alpha = "k":
+    case alpha = "l":
+    case alpha = "m":
+    case alpha = "n":
+    case alpha = "o":
+    case alpha = "p":
+    case alpha = "q":
+    case alpha = "r":
+    case alpha = "s":
+    case alpha = "t":
+    case alpha = "u":
+    case alpha = "v":
+    case alpha = "w":
+    case alpha = "x":
+    case alpha = "y":
+    case alpha = "z":
+            
     document.write(alpha + " is Alphabet.");
     break;
     default:
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 1. vowel and consonent will show the entry.
 - in switch statement condation must be "=" only don't need "===" or "==" (switch)
 - if we use "===" or "==" in condation not gonna work (switch)
+- if we use "===" or "==" in condation not gonna work (switch)
 2. a character is alphabet or not check. 
 - 
