593b2df7ea0bcdb6d625446d895cae45b20e928f
imran <zunaeede@gmail.com>
2023-09-09 14:51:47 +0600

    main - add rule - switch statement - condation rule - in switch - statement

--- 1_vowel.js
+++ 1_vowel.js
@@ -1,10 +1,10 @@
 let num = prompt("Entera any Alphabet");
 
 switch (num){
-    case num == "a":
+    case num = "a":
         document.write("It is Vowel");
         break;
-    case num === "b":
+    case num = "b":
         document.write("Not vowel");
     default:
         document.write("Wrong Entry");    
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Basic_Problem_solve
 #### basic problem solving practice
+1. vowel and consonent test practice
+- in switch statement condation must be "=" only don't need "===" or "=="
