cefca7763656cab1c1f33a3775e47cc30622f2bc
imran <zunaeede@gmail.com>
2023-09-09 22:55:40 +0600

    main - upper case

--- 4_uppercase.js
+++ 4_uppercase.js
@@ -1,2 +1,33 @@
 let upper = prompt("Enter a Alphabet to upperCase");
 
+switch(upper){
+    case upper = "A":
+    case upper = "B":
+    case upper = "C":
+    case upper = "D":
+    case upper = "E":
+    case upper = "F":
+    case upper = "G":
+    case upper = "H":
+    case upper = "I":
+    case upper = "J":
+    case upper = "K":
+    case upper = "L":
+    case upper = "M":
+    case upper = "N":
+    case upper = "O":
+    case upper = "P":
+    case upper = "Q":
+    case upper = "R":
+    case upper = "S":
+    case upper = "T":
+    case upper = "U":
+    case upper = "V":
+    case upper = "W":
+    case upper = "X":
+    case upper = "Y":
+    case upper = "Z":
+
+    document.write(upper + " is upper case");    
+    break;
+}
