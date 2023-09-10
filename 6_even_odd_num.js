a69bcaa14784a6ddac5d235d28baa0d8c7679557
imran <zunaeede@gmail.com>
2023-09-10 18:45:49 +0600

    main - add else condation

--- 6_even_odd_num.js
+++ 6_even_odd_num.js
@@ -4,4 +4,6 @@ if(num%2 === 0){
     document.write(num + "  Number is Even");
 }else if(num%2 === 1){
     document.write(num + "  Number is Odd");
+}else{
+    document.write("Enter any Number Not Alphabet");
 }
