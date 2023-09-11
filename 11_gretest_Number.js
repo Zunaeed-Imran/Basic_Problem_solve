02b60ca0ce745d03d165f1bde1948f27e4db6d70
imran <zunaeede@gmail.com>
2023-09-11 13:18:05 +0600

    main - big number will show

---
+++ 11_gretest_Number.js
@@ -0,0 +1,10 @@
+// Greatest of two numbers
+
+let num1 = prompt("Enter 1st Number");
+let num2 = prompt("Enter 2nd Number");
+
+if(num1 >= num2){
+    document.write(num1 + " is Greatest Number");
+}else if(num1 <= num2){
+    document.write(num2 + " is Greatest Number");
+}
