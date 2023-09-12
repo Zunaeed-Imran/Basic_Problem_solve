10df1f9fbf62d5702bfa21fadd25ccc490299306
imran <zunaeede@gmail.com>
2023-09-12 17:39:50 +0600

    main - split note

--- 17_reverse.js
+++ 17_reverse.js
@@ -1,6 +1,6 @@
 let num = prompt("Enter a number");
 
-let spl = num.split("");
+let spl = num.split(""); // we split with only ""
 let re = spl.reverse();
 let result = re.join();
 document.write(result);
