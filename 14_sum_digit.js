d31f417c91424134798e324d5ae6c8910c3135c3
Zunaeed-Imran <zunaeede@gmail.com>
2023-09-13 10:00:57 +0600

    main - trying to make it sum of it

--- 14_sum_digit.js
+++ 14_sum_digit.js
@@ -1,6 +1,6 @@
 // For example, let the input number be 719. The sum of digits of 719 = 7 + 1 + 9 = 17
 
 let num = prompt("Enter any Number");
-const num1 = num.split(" ");
+const num1 = num.split("");
 
 document.write(num1);
