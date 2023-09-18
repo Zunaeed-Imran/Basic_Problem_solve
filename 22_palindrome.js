aaeb5accb8f5262c2688081890e484dd976c6eef
imran <zunaeede@gmail.com>
2023-09-18 21:17:38 +0600

    main - palindrome - comment in line

--- 22_palindrome.js
+++ 22_palindrome.js
@@ -5,6 +5,9 @@ let spl = num.split('');
 let rev = spl.reverse();
 let join = rev.join();
 
+document.write(num + '<br>');   // for checking out put
+document.write(join + '<br>');  // for checking output
+
 if (num === join) {
   document.write(num + ' is Palindrome Number');
 } else {
