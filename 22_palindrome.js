a874d3b13d63ad872080ee03f6a0ca9186a84a60
imran <zunaeede@gmail.com>
2023-09-18 19:28:14 +0600

    main - palindrome number

--- 22_palindrome.js
+++ 22_palindrome.js
@@ -1,4 +1,5 @@
 // Making a program Palindrome or not
 
-let num = prompt("Enter a number to see palindrome or not");
+let num = prompt('Enter a number to see palindrome or not');
 
+document.write(num);
