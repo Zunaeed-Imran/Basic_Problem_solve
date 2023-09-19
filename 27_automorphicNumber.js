a39f6035e9b3d866615128fadd2ed546631d4410
imran <zunaeede@gmail.com>
2023-09-19 11:32:55 +0600

    main - add some program  file - not done now - will work with it later

---
+++ 27_automorphicNumber.js
@@ -0,0 +1,3 @@
+// this program will show tha number is automorphic or not.
+
+let num = prompt('Enter a number to see Automorphic Number or Not');
---
+++ 28_harshad_number.js
@@ -0,0 +1,3 @@
+// this program will see harshad Number or not
+
+let num = prompt('Enter a Number to see Harshed Number or not');
