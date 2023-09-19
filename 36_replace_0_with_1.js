926ecd90ad7b1178a3cf80e984cd54ff88aabb3a
imran <zunaeede@gmail.com>
2023-09-19 21:14:11 +0600

    main - not done  yet

--- 36_replace_0_with_1.js
+++ 36_replace_0_with_1.js
@@ -1,3 +1,10 @@
-// this program will all the input "zero" woth "one" 
+// this program will replace all the input "zero" woth "one"
 
 let num = prompt("Enter a 'zero' to make it 'one'");
+
+let spl = num.split('');
+
+switch (spl) {
+}
+
+document.write(spl);
