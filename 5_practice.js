16deb7bfecd1ff2554ccd7643cb102a91628ddab
imran <zunaeede@gmail.com>
2023-09-20 19:19:23 +0600

    main - digit count

--- 5_practice.js
+++ 5_practice.js
@@ -2,9 +2,7 @@
 
 let num = prompt('Enter number to see digit count.');
 
-let arr = [];
 
-for (let i = 0; i <= num; i++) {
-  arr[i] = prompt('Enter number');
-}
-document.write(arr);
+let result = num.length;
+
+document.write(result);
