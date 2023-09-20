017d6d9a6ec9390823f8522513c7b24c0b3d9cb9
Zunaeed-Imran <zunaeede@gmail.com>
2023-09-20 20:08:52 +0600

    main - done task - number of digit in an intiger

--- 13_number_of_digit.js
+++ 13_number_of_digit.js
@@ -1,11 +1,9 @@
+// Number of digits in an integer
+
 // The program to count a number of digits in an integer is discussed here. An integer is given as input and the total number of digits in that integer is displayed as output.
+
 
 let num = prompt('Enter number to see digit count.');
 
-let x;
-
-for (let i = 0; i <= num[i]; i++) {
-  x = i;
-  console.log(i);
-}
-document.write(num);
+let result = num.length;
+document.write(result);
