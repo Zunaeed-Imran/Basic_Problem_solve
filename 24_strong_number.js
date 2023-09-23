597c020056fedbca51988941a3b5b5fc4e1b8eac
imran <zunaeede@gmail.com>
2023-09-23 17:07:24 +0600

    main - not done  24 No - strong number

--- 24_strong_number.js
+++ 24_strong_number.js
@@ -1,4 +1,7 @@
 // making a program can say the number is strong number or not
 
-let num = prompt("Enter a number to see strong or not");
+let num = prompt('Enter a number to see strong or not');
 
+for (let i = num; i >= 1; i--) {
+  document.write(i + ' ');
+}
