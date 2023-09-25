c810e0332452b3992139a252dfbd36c706b2b6a1
imran <zunaeede@gmail.com>
2023-09-25 13:31:04 +0600

    main - not done still trying - GCD if 2 number

--- 10_GCD_two_number.js
+++ 10_GCD_two_number.js
@@ -3,18 +3,15 @@
 let num1 = prompt('Enter 1st number for GCD');
 let num2 = prompt('Enter 2nd number for GCD');
 
-let res1 = 0;
+let res1 = 1;
 
 for (let i = 0; i <= num1; i++) {
   if (num1 % i === 0) {
-    
         for (let j = 0; j <= num2; j++) {
-          if (num2 % j === 0) {
-            if (j === i) {
-              document.write(res1 + " is GCD of those number" + "<br>");
-            }
-          }
-        }
-
+          if (num2 % j === 0) {         
+            res1 = j.match(i);
+     }
+    }
+   }
   }
-}
+document.write(res1);
