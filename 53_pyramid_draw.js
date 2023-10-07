e1d5eb655a5d11b2e717242eda5b1ad0563da2fc
imran <zunaeede@gmail.com>
2023-10-07 09:17:02 +0600

    main - pyramid trying not done

--- 53_pyramid_draw.js
+++ 53_pyramid_draw.js
@@ -1,3 +1,13 @@
 // this program will draw a Pyramid pattern using stars
 
 
+for (let i = 0; i < 20; i++){
+  document.write("1");
+  for (let j = 0; j < 20; j++){
+    document.write("2");
+    for (let k = 0; k < 20; k++){
+      document.write("3");
+    }
+  }
+  document.write("<br>")
+}
