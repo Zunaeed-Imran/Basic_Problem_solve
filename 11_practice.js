16b35eaebbcc68bd992b8f209b7b7cfd4402f81d
imran <zunaeede@gmail.com>
2023-10-07 12:32:07 +0600

    practice - it worked - vowel and consonent

--- 11_practice.js
+++ 11_practice.js
@@ -5,7 +5,7 @@ let input = prompt("Enter Alphabet");
 for (var i = 0; i <= input.length; i++){
 
 }
-if (input.length === "a" | "e" | "i" | "o" | "u") {
+if (input === "a" | "e" | "i" | "o" | "u") {
   document.write(input + " is vowel");
 } else if(input.length == "b" || "c" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "m" || "n" || "p" || "q" || "r" || "s" || "t" || "v" || "w" || "x" || "y" || "z") {
   document.write(input + " is consonent");
