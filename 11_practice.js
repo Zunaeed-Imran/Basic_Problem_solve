27a389b52fd36a700d476ec17df884297874efc0
imran <zunaeede@gmail.com>
2023-10-07 12:42:51 +0600

    practice - it finally worked

--- 11_practice.js
+++ 11_practice.js
@@ -1,13 +1,14 @@
 // vowel and consonent practicing
 
+
 let input = prompt("Enter Alphabet");
 
 for (var i = 0; i <= input.length; i++){
 
 }
-if (input === "a" | "e" | "i" | "o" | "u") {
+if (input === "a" || input=== "e" || input === "i" || input === "o" || input === "u") {
   document.write(input + " is vowel");
-} else if(input.length == "b" || "c" || "d" || "f" || "g" || "h" || "j" || "k" || "l" || "m" || "n" || "p" || "q" || "r" || "s" || "t" || "v" || "w" || "x" || "y" || "z") {
+} else if(input === "b" || input === "c" || input === "d" || input === "f" || input === "g" || input === "h" || input === "j" || input === "k" || input === "l" || input === "m" || input === "n" || input === "p" || input === "q" || input === "r" || input === "s" || input === "t" || input === "v" || input === "w" || input ===  "x" || input === "y" || input === "z") {
   document.write(input + " is consonent");
 } else {
   document.write(input + " Not valid input");
