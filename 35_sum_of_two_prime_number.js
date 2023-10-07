cc83089fdb961c65f53d1237c0fa0927051bf0b7
imran <zunaeede@gmail.com>
2023-10-07 20:01:32 +0600

    main - trying to solved

--- 35_sum_of_two_prime_number.js
+++ 35_sum_of_two_prime_number.js
@@ -1,4 +1,12 @@
 // this program will express as a sum of two prime number.
 
-let num1 = prompt('Enter 1st prime number');
-let num2 = prompt('Enter 2nd prime number');
+let num = prompt('Enter number');
+
+
+for (var i = 1; i <= num; i++) {
+}
+if (num % num === 0 && num > 1 && num % 2 != 0) {
+  document.write(num + ' is Prime Number.');
+} else {
+  document.write(num + ' is Not a Prime Number.');
+}
