90165e5fbdb8ac9ecbc65b575f111bd7bd6355d9
imran <zunaeede@gmail.com>
2023-10-08 11:38:24 +0600

    practice - draw triangle practice

---
+++ 12_practice_pattern.js
@@ -0,0 +1,67 @@
+// practice pattern programming
+
+
+
+
+
+
+
+
+
+
+    document.write(" "+" "+" "+"*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write(" ");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("<br>");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+    document.write("*");
+ 
