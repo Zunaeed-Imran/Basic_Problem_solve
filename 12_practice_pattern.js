95332dc1424d7a24c774fc42facc9fbf67c9c5fd
imran <zunaeede@gmail.com>
2023-10-09 08:59:44 +0600

    practice - practice

--- 12_practice_pattern.js
+++ 12_practice_pattern.js
@@ -64,4 +64,17 @@
     document.write("*");
     document.write("*");
     document.write("*");
- 
+    document.write("*");
+    document.write("<br>");
+    document.write("<br>");
+    document.write("<br>");
+
+
+
+
+    for (let i = 0; i < 20; i++) {  1
+        for (let j = i +9; j > 12+9; j--) {
+            document.write('*');
+          }
+        document.write('<br>');
+        }
