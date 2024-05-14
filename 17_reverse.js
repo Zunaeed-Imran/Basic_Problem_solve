81eb3978b4c98067113618bc7d26969dd7881fcb
imran <zunaeede@gmail.com>
2024-05-14 13:04:52 +0600

    main - fix the reverse number problem.

--- 17_reverse.js
+++ 17_reverse.js
@@ -2,5 +2,5 @@ let num = prompt("Enter a number");
 
 let spl = num.split(""); // we split with only ""
 let re = spl.reverse();
-let result = re.join();
+let result = re.join("");
 document.write(result);
